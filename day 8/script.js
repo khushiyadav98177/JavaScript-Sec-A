b078bc862effbdebfae4b60565b67d3757881714
Khushi yadav <khushiyadav98177@gmail.com>
2026-02-04 11:00:50 +0530

    day 9

---
+++ day 8/script.js
@@ -0,0 +1,13 @@
+// const query = document.querySelector("#content p");
+// console.log(query);
+
+// query.textContent = "wertyui asdfghj wertyu zxcvbn sdf  rty yui"
+
+const query = document.querySelector("#content p");
+console.log(query);
+
+for(let i = 0; i <= query.length-1; i++) {
+    query.textContent = "wertyui asdfghj wertyu zxcvbn sdf  rty yui"
+}
+
+// query.textContent = "wertyui asdfghj wertyu zxcvbn sdf  rty yui"
