28012ea4bb11483a5baeda8f709bffc4b961fe0b
Khushi yadav <khushiyadav98177@gmail.com>
2026-02-06 20:26:09 +0530

    day 10

---
+++ day 10/script.js
@@ -0,0 +1,17 @@
+//function handleClick(){
+   // document.querySelector("#head"). 
+   // atyle.color="red";
+
+   // document.getElementById("btn"). 
+    //textContent="color changed"}
+
+
+const head =document.getElementById('head');{
+    const image = document.createElement("img");
+image.setAttribute("src","https://tse1.mm.bing.net/th/id/OIP.5IddxFlKEN4HOwmluPPdmwAAAA?rs=1&pid=ImgDetMain&o=7&rm=3");
+image.setAttribute("alt", "kr manglam");
+
+document.getElementById("content");
+
+document.getElementById("content").append(image);
+}
